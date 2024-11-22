db4f86f1637e2989b50a9655c4c0f8a51d76506c
Pieter Stevens <122227977+Psteven5@users.noreply.github.com>
2024-11-22 17:57:09 +0100

    Update rand.py

--- rand.py
+++ rand.py
@@ -20,6 +20,19 @@ class Hanoi(ACTR):
             x = "_"
 
         return x, y
+    
+    def repr_peg(self, peg, disks):
+        out = peg + " has disks ["
+        if '_' == disks[0]:
+            return out + ']'
+        for disk in disks:
+            if disk != disks[0]:
+                out += ", "
+            out += disk
+        return out + ']'
+    
+    def repr_pegs(self, a, b, c):
+        print("Peg " + self.repr_peg('A', a) + ", peg " + self.repr_peg('B', b) + ", peg " + self.repr_peg('C', c) + ".\n")
 
     def ab(goal="A:?a B:?b C:?c A:!_ C:!123", imaginal="Prev:!BA"):
         if b == "_" or a[-1] > b[-1]:
@@ -27,9 +40,7 @@ class Hanoi(ACTR):
             a, b = self.move(a, b)
             goal.modify(A=a, B=b)
             imaginal.modify(Prev="AB")
-            print(
-                "Peg A has disks {}, peg B has disks {}, peg C has disks {}.\n"
-            ).format(a, b, c)
+            self.repr_pegs(a, b, c)
 
     def ac(goal="A:?a B:?b C:?c A:!_ C:!123", imaginal="Prev:!CA"):
         if c == "_" or a[-1] > c[-1]:
@@ -37,9 +48,7 @@ class Hanoi(ACTR):
             a, c = self.move(a, c)
             goal.modify(A=a, C=c)
             imaginal.modify(Prev="AC")
-            print(
-                "Peg A has disks {}, peg B has disks {}, peg C has disks {}.\n"
-            ).format(a, b, c)
+            self.repr_pegs(a, b, c)
 
     def ba(goal="A:?a B:?b C:?c B:!_ C:!123", imaginal="Prev:!AB"):
         if a == "_" or b[-1] > a[-1]:
@@ -47,9 +56,7 @@ class Hanoi(ACTR):
             b, a = self.move(b, a)
             goal.modify(B=b, A=a)
             imaginal.modify(Prev="BA")
-            print(
-                "Peg A has disks {}, peg B has disks {}, peg C has disks {}.\n"
-            ).format(a, b, c)
+            self.repr_pegs(a, b, c)
 
     def bc(goal="A:?a B:?b C:?c B:!_ C:!123", imaginal="Prev:!CB"):
         if c == "_" or b[-1] > c[-1]:
@@ -57,9 +64,7 @@ class Hanoi(ACTR):
             b, c = self.move(b, c)
             goal.modify(B=b, C=c)
             imaginal.modify(Prev="BC")
-            print(
-                "Peg A has disks {}, peg B has disks {}, peg C has disks {}.\n"
-            ).format(a, b, c)
+            self.repr_pegs(a, b, c)
 
     def ca(goal="A:?a B:?b C:?c C:!_ C:!123", imaginal="Prev:!AC"):
         if a == "_" or c[-1] > a[-1]:
@@ -67,9 +72,7 @@ class Hanoi(ACTR):
             c, a = self.move(c, a)
             goal.modify(C=c, A=a)
             imaginal.modify(Prev="CA")
-            print(
-                "Peg A has disks {}, peg B has disks {}, peg C has disks {}.\n"
-            ).format(a, b, c)
+            self.repr_pegs(a, b, c)
 
     def cb(goal="A:?a B:?b C:?c C:!_ C:!123", imaginal="Prev:!BC"):
         if b == "_" or c[-1] > b[-1]:
@@ -77,17 +80,15 @@ class Hanoi(ACTR):
             c, b = self.move(c, b)
             goal.modify(C=c, B=b)
             imaginal.modify(Prev="CB")
-            print(
-                "Peg A has disks {}, peg B has disks {}, peg C has disks {}.\n"
-            ).format(a, b, c)
-
+            self.repr_pegs(a, b, c)
+            
     def final(goal="C:123"):
         goal.clear()
         goal.clear()
 
 
 model = Hanoi()
-ccm.log_everything(model)
+# ccm.log_everything(model)
 model.goal.set("A:123 B:_ C:_")
 model.imaginal.set("Prev:")
 model.run()
